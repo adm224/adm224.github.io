49d3e47822eac447185c88f0380861267448ba91
adm224 <159033007+adm224@users.noreply.github.com>
2025-04-02 18:02:18 -0400

    Update style.js
    First draft of function

--- style.js
+++ style.js
@@ -1,1 +1,8 @@
+function showIt(var1){
+	//hide the one div that is shown by hiding all
+document.getElementById('divvy1').style.display = 'none';
+document.getElementById('divvy2').style.display = 'none';
+	//show the one div that you sent to this function
+document.getElementById(var1).style.display = 'block';
 
+}
