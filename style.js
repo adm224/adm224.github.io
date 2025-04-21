5ebc084cf176e351923ad17c1f24038426f5a48e
adm224 <159033007+adm224@users.noreply.github.com>
2025-04-21 15:21:16 -0400

    Update style.js
    Added 2 lessons to js

--- style.js
+++ style.js
@@ -3,6 +3,8 @@ function showIt(var1){
 document.getElementById('divvy1').style.display = 'none';
 document.getElementById('divvy2').style.display = 'none';
 document.getElementById('divvy3').style.display = 'none';
+document.getElementById('divvy4').style.display = 'none';
+document.getElementById('divvy5').style.display = 'none';
 	//show the one div that you sent to this function
 document.getElementById(var1).style.display = 'block';
 
