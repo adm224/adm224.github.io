5af160d6cdf19ae0d7d1be1ec886246de8df2edd
adm224 <159033007+adm224@users.noreply.github.com>
2025-04-25 23:08:00 -0400

    Update style.js

--- style.js
+++ style.js
@@ -1,3 +1,59 @@
+/* function showIt(var1){
+  //hide the one div that is shown by hiding all
+document.getElementById('divvy1').style.display = 'none';
+document.getElementById('divvy2').style.display = 'none';
+document.getElementById('divvy3').style.display = 'none';
+document.getElementById('divvy4').style.display = 'none';
+document.getElementById('divvy5').style.display = 'none';
+  //show the one div that you sent to this function
+document.getElementById(var1).style.display = 'block';
+
+} */
+
+/* function changeIt(var1){
+document.getElementByClassName('divvy1').class = '15px';
+document.getElementById('divvy2').style.width = '15px';
+document.getElementById('divvy3').style.width = '15px';
+document.getElementById('divvy4').style.width = '15px';
+document.getElementById('divvy5').style.width = '15px';
+document.getElementById(var1).style.width = '100px';
+
+}
+ */
+/* document.querySelector('.circleBeta').style.width = '30px';
+
+function toggle() {
+thing=getElementByID('divvy1');
+if (thing.className === "thisClass thatClass thirdClass") {
+thing.className === "thisClassTwo thatClass thirdClass"
+}
+else 
+{
+thing.className === "thisClass thatClass thirdClass"
+}
+ */
+
+
+/* var var1 = document.querySelector(':root');
+function change() {
+  var1.style.setProperty('--animation1', 'animation2'); */
+
+/* THIS WASN'T WORKING, SO I DID A NEW THINK */
+/* let start = true;
+
+function changeIt() {   
+  const root = document.documentElement;
+
+  if(start) {
+    root.style.setProperty('--animation1', 'circleRibbon 2s linear infinite');
+    start = false;  
+  } else {
+    root.style.setProperty('--animation1', 'circleRibbon 20s linear infinite');
+    initialTheme = true;
+  }
+}
+ */
+ 
 let slow = true;
 
 function changeD() {
@@ -10,17 +66,11 @@ let wid = true;
 
 function changeW() {
   const root = document.documentElement;
-  root.style.setProperty('--width', wid ? '1px' : '15px');
+  root.style.setProperty('--width', wid ? '1' : '15px');
   wid = !wid;
 }
 
 let hie = true;
-
-function changeH() {
-  const root = document.documentElement;
-  root.style.setProperty('--height', hie ? '1px' : '15px');
-  hie = !hie;
-}
 
 let bor = true;
 
@@ -49,8 +99,8 @@ function changeBC() {
 
 let nam = true;
 
-function changeN() {
+/* function changeN() {
   const root = document.documentElement;
   root.style.setProperty('--animationName', nam ? 'none' : 'circleRibbon');
   nam = !nam;
-}
+} */
