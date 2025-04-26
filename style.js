9157e44d7d23aea37385da904ebc72e710b1c0b0
adm224 <159033007+adm224@users.noreply.github.com>
2025-04-25 21:58:08 -0400

    Update style.js
    Submission JS

--- style.js
+++ style.js
@@ -1,11 +1,56 @@
-function showIt(var1){
-	//hide the one div that is shown by hiding all
-document.getElementById('divvy1').style.display = 'none';
-document.getElementById('divvy2').style.display = 'none';
-document.getElementById('divvy3').style.display = 'none';
-document.getElementById('divvy4').style.display = 'none';
-document.getElementById('divvy5').style.display = 'none';
-	//show the one div that you sent to this function
-document.getElementById(var1).style.display = 'block';
+let slow = true;
 
+function changeD() {
+  const root = document.documentElement;
+  root.style.setProperty('--animationDuration', slow ? '2s' : '20s');
+  slow = !slow;
+}
+
+let wid = true;
+
+function changeW() {
+  const root = document.documentElement;
+  root.style.setProperty('--width', wid ? '1px' : '15px');
+  wid = !wid;
+}
+
+let hie = true;
+
+function changeH() {
+  const root = document.documentElement;
+  root.style.setProperty('--height', hie ? '1px' : '15px');
+  hie = !hie;
+}
+
+let bor = true;
+
+function changeB() {
+  const root = document.documentElement;
+  root.style.setProperty('--border', bor ? 'solid black' : 'none');
+  bor = !bor;
+}
+
+let pos = true;
+
+function changePT() {
+  const root = document.documentElement;
+  root.style.setProperty('--position', pos ? 'absolute' : 'relative');
+  root.style.setProperty('--top', pos ? '50%' : '3%');
+  pos = !pos;
+}
+
+let bac = true;
+
+function changeBC() {
+  const root = document.documentElement;
+  root.style.setProperty('--background-color', bac ? 'red' : 'black');
+  bac = !bac;
+}
+
+let nam = true;
+
+function changeN() {
+  const root = document.documentElement;
+  root.style.setProperty('--animationName', nam ? 'none' : 'circleRibbon');
+  nam = !nam;
 }
