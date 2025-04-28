ffc1e144e637dbdc9678b30a201ed44931109cd5
adm224 <159033007+adm224@users.noreply.github.com>
2025-04-28 15:17:40 -0400

    Update style.js

--- style.js
+++ style.js
@@ -1,10 +1,18 @@
+let box = true;
 
- 
+function changePT() {
+  const root = document.documentElement;
+  root.style.setProperty('--boxShadow', box ? '10px 1px' : 'none');
+  document.getElementById("b1").style.backgroundColor = box ? "blue" : "transparent";
+  box = !box;
+}
+
 let slow = true;
 
 function changeD() {
   const root = document.documentElement;
   root.style.setProperty('--animationDuration', slow ? '60s' : '20s');
+  document.getElementById("b2").style.backgroundColor = slow ? "gold" : "transparent";
   slow = !slow;
 }
 
@@ -12,26 +20,18 @@ let wid = true;
 
 function changeW() {
   const root = document.documentElement;
-  root.style.setProperty('--width', wid ? 'fill' : '15px');
+  root.style.setProperty('--width', wid ? 'fill' : "30px");
+  document.getElementById("b3").style.backgroundColor = wid ? "purple" : "transparent";
   wid = !wid;
 }
-
-let hie = true;
 
 let bor = true;
 
 function changeB() {
   const root = document.documentElement;
   root.style.setProperty('--border', bor ? 'solid white' : 'none');
+  document.getElementById("b4").style.backgroundColor = bor ? "green" : "transparent";
   bor = !bor;
-}
-
-let box = true;
-
-function changePT() {
-  const root = document.documentElement;
-  root.style.setProperty('--boxShadow', box ? '10px 1px' : 'none');
-  box = !box;
 }
 
 let bac = true;
@@ -39,13 +39,7 @@ let bac = true;
 function changeBC() {
   const root = document.documentElement;
   root.style.setProperty('--background-color', bac ? 'red' : 'white');
+  document.getElementById("b5").style.backgroundColor = bac ? "red" : "transparent";
   bac = !bac;
 }
 
-let nam = true;
-
-/* function changeN() {
-  const root = document.documentElement;
-  root.style.setProperty('--animationName', nam ? 'none' : 'circleRibbon');
-  nam = !nam;
-} */
