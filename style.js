fcc05380c1517546970143479662a14896749e08
adm224 <159033007+adm224@users.noreply.github.com>
2025-05-08 00:42:13 -0400

    Update style.js

--- style.js
+++ style.js
@@ -1,45 +1,45 @@
 let box = true;
 
-function changePT() {
+function changePT(var1) {
   const root = document.documentElement;
   root.style.setProperty('--boxShadow', box ? '10px 1px' : 'none');
-  document.getElementById("b1").style.backgroundColor = box ? "blue" : "transparent";
+  var1.style.backgroundColor = box ? "blue" : "transparent";
   box = !box;
 }
 
 let slow = true;
 
-function changeD() {
+function changeD(var2) {
   const root = document.documentElement;
-  root.style.setProperty('--animationDuration', slow ? '60s' : '20s');
-  document.getElementById("b2").style.backgroundColor = slow ? "gold" : "transparent";
+  root.style.setProperty('--height', slow ? '5px' : '15px');
+  var2.style.backgroundColor = slow ? "gold" : "transparent";
   slow = !slow;
 }
 
 let wid = true;
 
-function changeW() {
+function changeW(var3) {
   const root = document.documentElement;
-  root.style.setProperty('--width', wid ? 'fill' : "30px");
-  document.getElementById("b3").style.backgroundColor = wid ? "purple" : "transparent";
+  root.style.setProperty('--width', wid ? 'fill' : "15px");
+  var3.style.backgroundColor = wid ? "purple" : "transparent";
   wid = !wid;
 }
 
 let bor = true;
 
-function changeB() {
+function changeB(var4) {
   const root = document.documentElement;
   root.style.setProperty('--border', bor ? 'solid white' : 'none');
-  document.getElementById("b4").style.backgroundColor = bor ? "green" : "transparent";
+  var4.style.backgroundColor = bor ? "green" : "transparent";
   bor = !bor;
 }
 
 let bac = true;
 
-function changeBC() {
+function changeBC(var5) {
   const root = document.documentElement;
   root.style.setProperty('--background-color', bac ? 'red' : 'white');
-  document.getElementById("b5").style.backgroundColor = bac ? "red" : "transparent";
+  var5.style.backgroundColor = bac ? "red" : "transparent";
   bac = !bac;
 }
 
